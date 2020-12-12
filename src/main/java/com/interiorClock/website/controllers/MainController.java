9802c9854a72e11e1b1e93936f4e6c90c4689f50
KarinaShik <63586318+KarinaShik@users.noreply.github.com>
2020-12-12 22:16:12 +0300

    feat: mapping HTTP GET requests

--- src/main/java/com/interiorClock/website/controllers/MainController.java
+++ src/main/java/com/interiorClock/website/controllers/MainController.java
@@ -13,4 +13,57 @@ public class MainController {
         return "index";
     }
 
+    @GetMapping("/shop")
+    public String Shop(Model model) {
+        model.addAttribute("title", "Shop");
+        return "shop";
+    }
+
+    @GetMapping("/product")
+    public String Product(Model model) {
+        model.addAttribute("title", "Product");
+        return "product-details";
+    }
+
+    @GetMapping("/wishlist")
+    public String Wishlist(Model model) {
+        model.addAttribute("title", "Wishlist");
+        return "wishlist";
+    }
+
+    @GetMapping("/compare")
+    public String Compare(Model model) {
+        model.addAttribute("title", "Compare");
+        return "compare-page";
+    }
+
+    @GetMapping("/cart")
+    public String Cart(Model model) {
+        model.addAttribute("title", "CartPage");
+        return "cart-page";
+    }
+
+    @GetMapping("/checkout")
+    public String Checkout(Model model) {
+        model.addAttribute("title", "Checkout");
+        return "checkout";
+    }
+
+    @GetMapping("/contact")
+    public String Contact(Model model) {
+        model.addAttribute("title", "Contact us");
+        return "contact-us";
+    }
+
+    @GetMapping("/account")
+    public String Account(Model model) {
+        model.addAttribute("title", "Account");
+        return "my-account";
+    }
+
+    @GetMapping("/authorization")
+    public String Authorization(Model model) {
+        model.addAttribute("title", "Authorization");
+        return "login-register";
+    }
 }
