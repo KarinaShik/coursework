1c8d78994884d1ab38cbcefcb4106f7c02313a45
KarinaShik <63586318+KarinaShik@users.noreply.github.com>
2020-12-17 23:44:22 +0300

    feat: add findAllByCategory() method

--- src/main/java/com/interiorClock/website/repo/ClockRepository.java
+++ src/main/java/com/interiorClock/website/repo/ClockRepository.java
@@ -4,6 +4,10 @@ import com.interiorClock.website.entities.ClockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClockRepository extends JpaRepository<ClockEntity, Integer> {
+
+    List<ClockEntity> findAllByCategory(String category, org.springframework.data.domain.Sort sort);
 }
