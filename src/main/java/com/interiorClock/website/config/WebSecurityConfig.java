36b04863d661d34e127f3a518a740ec2525dee07
KarinaShik <63586318+KarinaShik@users.noreply.github.com>
2020-12-17 23:45:29 +0300

    feat: add spring security

---
+++ src/main/java/com/interiorClock/website/config/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.interiorClock.website.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.GET, "/**/*.css", "/**/*.js", "/**/*.scss",
+                        "/**/*.png", "/**/*.svg", "/**/*.gif",
+                        "/**/*.woff", "/**/*.woff2", "/**/*.ttf").permitAll()
+                    .antMatchers("/", "/shop", "/shop/product/{id}","/contact").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                        .loginPage("/authorization")
+                        .permitAll()
+                .and()
+                    .logout()
+                        .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails admin =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("123")
+                        .roles("ADMIN")
+                        .build();
+
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("123")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+}
